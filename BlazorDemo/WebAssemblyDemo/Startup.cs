103fd1cd7b724b6946bfa7adb8e17b0432d0fb35
Juan Manuel Orjuela <orjuela.juanmanuel@gmail.com>
2018-12-26 19:36:42 -0500

    Add demo web assembly

---
+++ BlazorDemo/WebAssemblyDemo/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAssemblyDemo
+{
+    using Microsoft.AspNetCore.StaticFiles;
+
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var provider = new FileExtensionContentTypeProvider();
+            provider.Mappings[".wasm"] = "application/octet-stream";
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                ContentTypeProvider = provider
+            });
+        }
+    }
+}
---
+++ BlazorDemo/WebAssemblyDemo/wwwroot/sumValues.c
@@ -0,0 +1,4 @@
+﻿int sumValues(int value1, int value2) 
+{
+	return value1 + value2;
+}
